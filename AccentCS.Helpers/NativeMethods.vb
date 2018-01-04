630a9e28472d611ba759fbde85bc8ea9197495c3
mcdenis <maxecole@hotmail.com>
2018-01-04 15:39:50 -0500

    Restore default colors from the current visual style

--- AccentCS.Helpers/NativeMethods.vb
+++ AccentCS.Helpers/NativeMethods.vb
@@ -3,12 +3,16 @@
 Module NativeMethods
 
     <DllImport("user32.dll", SetLastError:=True)>
-    Friend Function SetSysColors(ByVal nChanges As Integer,
+    Public Function SetSysColors(ByVal nChanges As Integer,
            ByRef lpSysColor As Integer, ByRef lpColorValues As Integer) As Integer
     End Function
 
+    <DllImport("uxtheme.dll", ExactSpelling:=True)>
+    Public Function GetThemeSysColor(hTheme As IntPtr, iColorId As Integer) As Integer
+    End Function
+
     <DllImport("dwmapi.dll", PreserveSig:=False)>
-    Friend Sub DwmGetColorizationColor(ByRef pcrColorization As Integer, <MarshalAs(UnmanagedType.Bool)> ByRef pfOpaqueBlend As Boolean)
+    Public Sub DwmGetColorizationColor(ByRef pcrColorization As Integer, <MarshalAs(UnmanagedType.Bool)> ByRef pfOpaqueBlend As Boolean)
     End Sub
 
 End Module
